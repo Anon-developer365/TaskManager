da8056482bc3d7dd712f92e13b84eb2e6e86a299
Anon-Developer365 <Anon-Developer365>
2025-04-18 14:24:11 +0100

    feat(task): pojo added
    Signed-off-by: Anon-Developer365 <Anon-Developer365>

---
+++ src/main/java/org/example/taskmanager/controllers/Task.java
@@ -0,0 +1,21 @@
+package org.example.taskmanager.controllers;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * POJO for task object.
+ */
+
+@AllArgsConstructor
+@Data
+public class Task {
+    private String id;
+    private String caseNumber;
+    private String title;
+    private String description;
+    private String status;
+    private LocalDateTime createdDate;
+}
