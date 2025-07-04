fbf6306f7017e6b08ef2c4085d8d7a101368d114
AnonDeveloper <AnonDeveloper@github.co.uk>
2025-07-04 12:34:33 +0100

    feat(integration): cucumber
    cucumber dependancies added to add in cucumber integration testing.
    
    Signed-off-by: AnonDeveloper <AnonDeveloper@github.co.uk>

---
+++ src/integration-test/java/org.example.taskmanager/ApplicationIT.java
@@ -0,0 +1,6 @@
+package org.example.taskmanager;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = TaskManagerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApplicationIT {
+}
