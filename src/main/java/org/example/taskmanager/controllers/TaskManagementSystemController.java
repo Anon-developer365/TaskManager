68598fc485f165e665ce6ba7107ca6263b3517ec
AnonDeveloper <AnonDeveloper@github.co.uk>
2025-07-06 19:29:26 +0100

    refactor(tms): controller
    validated annotation added to ensure validation of user input.
    
    Signed-off-by: AnonDeveloper <AnonDeveloper@github.co.uk>

--- src/main/java/org/example/taskmanager/controllers/TaskManagementSystemController.java
+++ src/main/java/org/example/taskmanager/controllers/TaskManagementSystemController.java
@@ -4,6 +4,7 @@ import org.example.taskmanager.service.GetATask;
 import org.example.taskmanager.service.RetrieveTasks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,26 +34,26 @@ public class TaskManagementSystemController implements TaskManagementSystemApi {
 
     @Override
     @RequestMapping(value = "/Task", method = RequestMethod.POST)
-    public ResponseEntity<SuccessResponse> createTask(String transactionId, CreateTaskRequest taskRequest) {
+    public ResponseEntity<SuccessResponse> createTask(@Validated String transactionId, @Validated CreateTaskRequest taskRequest) {
 
             return ok(createTaskOrchestration.createTask(transactionId, taskRequest));
     }
 
     @Override
     @RequestMapping(value = "/Task", method = RequestMethod.GET)
-    public ResponseEntity<Task> getTask(String transactionId, String taskId) {
+    public ResponseEntity<Task> getTask(@Validated String transactionId, String taskId) {
         return ok(getATask.getATask(taskId));
     }
 
     @Override
     @RequestMapping(value = "/allTasks", method = RequestMethod.GET)
-    public ResponseEntity<TaskResponse> getTasks(String transactionId) {
+    public ResponseEntity<TaskResponse> getTasks(@Validated String transactionId) {
         return ok(getAllTasks.getAllTasks());
     }
 
     @Override
     @RequestMapping(value = "/Task", method = RequestMethod.PUT)
-    public ResponseEntity<SuccessResponse> updateStatus(String transactionId, UpdateStatusRequest body) {
+    public ResponseEntity<SuccessResponse> updateStatus(@Validated String transactionId, @Validated UpdateStatusRequest body) {
 
         return ok(updateStatusOrchestration.updateStatus(body));
     }
