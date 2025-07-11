eef6a4dbccec85a3e3a981e507593d46357ce8b8
AnonDeveloper <AnonDeveloper@github.co.uk>
2025-07-11 13:20:00 +0100

    feat(create): error
    create task error tests added to create task integration test.
    
    Signed-off-by: AnonDeveloper <AnonDeveloper@github.co.uk>

--- src/test/java/org/example/taskmanager/integrationTest/CreateTaskIntegrationTests.java
+++ src/test/java/org/example/taskmanager/integrationTest/CreateTaskIntegrationTests.java
@@ -2,7 +2,6 @@ package org.example.taskmanager.integrationTest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.taskmanager.TaskManagerApplication;
-import org.example.taskmanager.service.TaskRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -74,4 +73,188 @@ public class CreateTaskIntegrationTests {
                 .andReturn();
     }
 
+    @Test
+    void whenTaskStatusIsMissingAnErrorMessageIsReturned() throws Exception {
+        String stringDate = "2025-05-05 17:00";
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm", Locale.UK);
+        LocalDateTime dueDate = LocalDateTime.parse(stringDate, dateTimeFormatter);
+        CreateTaskRequest taskRequest = new CreateTaskRequest();
+        taskRequest.setTaskDescription("Description");
+        taskRequest.setTitle("Title");
+        taskRequest.setDueDate(dueDate);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("transactionId", "1");
+        String json = objectMapper.writeValueAsString(taskRequest);
+
+        mvc.perform(post("/Task").contentType(MediaType.APPLICATION_JSON)
+                        .headers(httpHeaders)
+                        .content(json))
+                .andExpect(status().is(400))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.errors").value("[Task status is empty]"))
+                .andReturn();
+    }
+
+    @Test
+    void whenTaskTitleIsMissingAnErrorMessageIsReturned() throws Exception {
+        String stringDate = "2025-05-05 17:00";
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm", Locale.UK);
+        LocalDateTime dueDate = LocalDateTime.parse(stringDate, dateTimeFormatter);
+        CreateTaskRequest taskRequest = new CreateTaskRequest();
+        taskRequest.setTaskDescription("Description");
+        taskRequest.setStatus("Status");
+        taskRequest.setDueDate(dueDate);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("transactionId", "1");
+        String json = objectMapper.writeValueAsString(taskRequest);
+
+        mvc.perform(post("/Task").contentType(MediaType.APPLICATION_JSON)
+                        .headers(httpHeaders)
+                        .content(json))
+                .andExpect(status().is(400))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.errors").value("[Task title is empty]"))
+                .andReturn();
+    }
+
+    @Test
+    void whenTaskDueDateIsMissingAnErrorMessageIsReturned() throws Exception {
+        CreateTaskRequest taskRequest = new CreateTaskRequest();
+        taskRequest.setTaskDescription("Description");
+        taskRequest.setStatus("Status");
+        taskRequest.setTitle("Title");
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("transactionId", "1");
+        String json = objectMapper.writeValueAsString(taskRequest);
+
+        mvc.perform(post("/Task").contentType(MediaType.APPLICATION_JSON)
+                        .headers(httpHeaders)
+                        .content(json))
+                .andExpect(status().is(400))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.errors").value("[Task due date is empty]"))
+                .andReturn();
+    }
+
+    @Test
+    void whenTaskDescriptionIsEmptyTheTaskIsSavedInTheDatabase() throws Exception {
+        String stringDate = "2025-05-05 17:00";
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm", Locale.UK);
+        LocalDateTime dueDate = LocalDateTime.parse(stringDate, dateTimeFormatter);
+        CreateTaskRequest taskRequest = new CreateTaskRequest();
+        taskRequest.setTitle("Title");
+        taskRequest.setStatus("Status");
+        taskRequest.setDueDate(dueDate);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("transactionId", "1");
+        String json = objectMapper.writeValueAsString(taskRequest);
+
+        mvc.perform(post("/Task").contentType(MediaType.APPLICATION_JSON)
+                        .headers(httpHeaders)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").value("Task Created Successfully"))
+                .andReturn();
+    }
+
+    @Test
+    void whenTaskTitleIsInAnIncorrectFormatAnErrorMessageIsReturned() throws Exception {
+        String stringDate = "2025-05-05 17:00";
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm", Locale.UK);
+        LocalDateTime dueDate = LocalDateTime.parse(stringDate, dateTimeFormatter);
+        CreateTaskRequest taskRequest = new CreateTaskRequest();
+        taskRequest.setTaskDescription("Description");
+        taskRequest.setStatus("Status");
+        taskRequest.setTitle("##+^&");
+        taskRequest.setDueDate(dueDate);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("transactionId", "1");
+        String json = objectMapper.writeValueAsString(taskRequest);
+
+        mvc.perform(post("/Task").contentType(MediaType.APPLICATION_JSON)
+                        .headers(httpHeaders)
+                        .content(json))
+                .andExpect(status().is(400))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.errors").value("[Task title does not match the pattern a-zA-Z0-9]"))
+                .andReturn();
+    }
+
+    @Test
+    void whenTaskStatusIsInAnIncorrectFormatAnErrorMessageIsReturned() throws Exception {
+        String stringDate = "2025-05-05 17:00";
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm", Locale.UK);
+        LocalDateTime dueDate = LocalDateTime.parse(stringDate, dateTimeFormatter);
+        CreateTaskRequest taskRequest = new CreateTaskRequest();
+        taskRequest.setTaskDescription("Description");
+        taskRequest.setStatus("##+^&");
+        taskRequest.setTitle("Title");
+        taskRequest.setDueDate(dueDate);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("transactionId", "1");
+        String json = objectMapper.writeValueAsString(taskRequest);
+
+        mvc.perform(post("/Task").contentType(MediaType.APPLICATION_JSON)
+                        .headers(httpHeaders)
+                        .content(json))
+                .andExpect(status().is(400))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.errors").value("[Task status does not match the pattern a-zA-Z0-9]"))
+                .andReturn();
+    }
+
+    @Test
+    void whenTaskDescriptionIsInAnIncorrectFormatAnErrorMessageIsReturned() throws Exception {
+        String stringDate = "2025-05-05 17:00";
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm", Locale.UK);
+        LocalDateTime dueDate = LocalDateTime.parse(stringDate, dateTimeFormatter);
+        CreateTaskRequest taskRequest = new CreateTaskRequest();
+        taskRequest.setTaskDescription("##+^&");
+        taskRequest.setStatus("Status");
+        taskRequest.setTitle("Title");
+        taskRequest.setDueDate(dueDate);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("transactionId", "1");
+        String json = objectMapper.writeValueAsString(taskRequest);
+
+        mvc.perform(post("/Task").contentType(MediaType.APPLICATION_JSON)
+                        .headers(httpHeaders)
+                        .content(json))
+                .andExpect(status().is(400))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.errors").value("[Task description does not match the pattern a-zA-Z0-9]"))
+                .andReturn();
+    }
+
+    @Test
+    void whenThereIsMoreThanOneErrorAllErrorsAreReturned() throws Exception {
+        String stringDate = "2025-05-05 17:00";
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm", Locale.UK);
+        LocalDateTime dueDate = LocalDateTime.parse(stringDate, dateTimeFormatter);
+        CreateTaskRequest taskRequest = new CreateTaskRequest();
+        taskRequest.setTaskDescription("##+^&");
+        taskRequest.setStatus("");
+        taskRequest.setTitle("Title");
+        taskRequest.setDueDate(dueDate);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("transactionId", "1");
+        String json = objectMapper.writeValueAsString(taskRequest);
+
+        mvc.perform(post("/Task").contentType(MediaType.APPLICATION_JSON)
+                        .headers(httpHeaders)
+                        .content(json))
+                .andExpect(status().is(400))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.errors").value("[Task description does not match the pattern a-zA-Z0-9, Task status is empty]"))
+                .andReturn();
+    }
 }
